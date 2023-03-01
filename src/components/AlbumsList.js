724ed227c53820222ab2346e0596cd9b9144d6e0
madoka <menomonies@gmail.com>
2023-03-01 13:22:56 -0800

    Add animation

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,13 +1,37 @@
 import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
   // we can just call this hook, then immediatery try to fetch some data
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>;
+
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
  
-  
   console.log(data, error, isLoading);
 
-  return <div>Albums for {user.name}</div>;
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
